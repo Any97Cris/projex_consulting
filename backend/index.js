366daebf00abea45d1078602a22854e27c4268d0
Any97Cris <criscianysilva1997@gmail.com>
2023-06-12 10:43:08 -0400

    config jwt e cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+const jwt = require("jsonwebtoken");
 const userRoute = require("./routes/user");
 const immobileRoute = require("./routes/immobile");
 
+app.use(cors());
+
+app.use(express.json());
 
 app.use("/user", userRoute);
 app.use("/immobile", immobileRoute);
