4fe05f37c59f4f7e9deac8901e122581d4532cc1
Any97Cris <criscianysilva1997@gmail.com>
2023-06-12 10:04:59 -0400

    rotas users

---
+++ backend/routes/user.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    res.send({data:"página user"});
+});
+
+router.post("/create", (req,res) => {
+    res.send({data:"página create"});
+});
+
+router.get("/id/",(req,res) => {
+    res.send({data: "encontrar por ID"});
+});
+
+router.put("/update", (req,res) => {
+    res.send({data: "págia editar"})
+});
+
+router.delete("/delete/", (req,res) => {
+    res.send({data: "página deletar"});
+})
+
+
+
+module.exports = router;
