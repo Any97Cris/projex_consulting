44e0c4065b6f548f7228836b8c183951f570494b
Any97Cris <criscianysilva1997@gmail.com>
2023-06-12 10:04:51 -0400

    config dependence

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("teste")
+const express = require("express");
+const app = express();
+const userRoute = require("./routes/user");
+const immobileRoute = require("./routes/immobile");
+
+
+app.use("/user", userRoute);
+app.use("/immobile", immobileRoute);
+  
+app.listen(3000, () => {
+    console.log("servidor rodando");
+})
