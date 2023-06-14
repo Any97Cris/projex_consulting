e850d5c31b045af6ad63c7f35c49b588b47957e6
Any97Cris <criscianysilva1997@gmail.com>
2023-06-14 16:10:37 -0400

    impot servicos angular que comunica com api

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListUserComponent } from './components/list-user/list-user.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FormUserComponent } from './components/form-user/form-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListUserComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FormUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
