b4350da250d7598ef1aec0056c948732f397f880
Any97Cris <criscianysilva1997@gmail.com>
2023-06-14 16:09:52 -0400

    config com a rota create api

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './User';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+
+  constructor(private userService: UserService){}
+
+  async createHandler(user: User){
+    const formData = new FormData()
+
+    formData.append('id', user.id)
+    formData.append('nome',user.nome)
+    formData.append('email', user.email)
+    formData.append('password', user.password)
+
+    await this.userService.createUser(formData).subscribe();
+
+  }
 }
